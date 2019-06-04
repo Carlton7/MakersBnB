02c488e228d92967d190dce1b05a7415dbdaf5a8
carlton anand <carlton.anand@gmail.com>
2019-06-04 15:20:45 +0100

    adding heroku database to our project

--- lib/database_connection_setup.rb
+++ lib/database_connection_setup.rb
@@ -1,9 +1,9 @@
 require 'data_mapper'
 
 if ENV['ENVIRONMENT'] == 'test'
-    DataMapper.setup(:default, 'postgres://:@localhost/makersbnb_test')
+    DataMapper.setup(:default, 'postgres://dyrsdwqtqglgpn:8e95b389326b8b3be42caba3599757387c01ae3932ecd2f4363428ba13989293@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d2cs65ngo20q49')
 else
-    DataMapper.setup(:default, 'postgres://:@localhost/makersbnb')
+    DataMapper.setup(:default, 'postgres://dyprngjwxidrct:5d716c3a1121d4b3b9217a87cd27b4d943af3f67235a6e8985c71f2c0aae58e1@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d1grbm419vri2u')
 end
 
 DataMapper.auto_upgrade!
--- spec/set_up_test_database.rb
+++ spec/set_up_test_database.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 def setup_test_db
 
-  DataMapper.setup(:default, 'postgres://:@localhost/makersbnb_test')
+  DataMapper.setup(:default, 'postgres://dyrsdwqtqglgpn:8e95b389326b8b3be42caba3599757387c01ae3932ecd2f4363428ba13989293@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d2cs65ngo20q49')
 
 end
 
